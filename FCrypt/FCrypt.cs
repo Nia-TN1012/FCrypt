c3be1e8f7224dab0e0bad5e9368bce027ba5e201
Nia Tomonaka <nia1012-tmnk@outlook.jp>
2019-07-22 18:00:18 +0900

    First commit.

---
+++ FCrypt/FCrypt.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace FCrypt {
+
+    /// <summary>
+    /// AES type
+    /// </summary>
+    public enum CipherType {
+        /// <summary>
+        /// AES 128bit
+        /// </summary>
+        AES128 = 16,
+        /// <summary>
+        /// AES 256bit
+        /// </summary>
+        AES256 = 32
+    }
+
+    /// <summary>
+    /// Encrypts and Decrypts file.
+    /// </summary>
+    public class FCrypt {
+
+        /// <summary>
+        /// Initial vector
+        /// </summary>
+        private static readonly byte[] aesIV;
+        /// <summary>
+        /// Salt value
+        /// </summary>
+        private static readonly byte[] keySalt;
+
+        /// <summary>
+        /// Initializes
+        /// </summary>
+        static FCrypt() {
+            var encoder = new UTF8Encoding();
+            var assembly = Assembly.GetExecutingAssembly();
+            using( Stream stream = assembly.GetManifestResourceStream( "FCrypt.option.xml" ) ) {
+                var xml = XDocument.Load( stream );
+                aesIV = encoder.GetBytes( xml.XPathSelectElement( "/cipher/iv" ).Value );
+                keySalt = encoder.GetBytes( xml.XPathSelectElement( "/cipher/salt" ).Value );
+            }
+        }
+
+        /// <summary>
+        /// Generates a key
+        /// </summary>
+        /// <param name="password">Password</param>
+        /// <param name="cipherType">Cipher type (AES 128bit or AES 256bit)</param>
+        /// <returns>Key for AES</returns>
+        private static byte[] GenerateKey( string password, CipherType cipherType ) => new Rfc2898DeriveBytes( password, keySalt, 1000 ).GetBytes( ( int )cipherType );
+
+        /// <summary>
+        /// Encrypts a file
+        /// </summary>
+        /// <param name="inputFilePath">Original file path</param>
+        /// <param name="outputFilePath">Encrypted file path</param>
+        /// <param name="password">Password</param>
+        /// <param name="cipherType">Cipher type (AES 128bit or AES 256bit)</param>
+        public static void Encrypt( string inputFilePath, string outputFilePath, string password, CipherType cipherType = CipherType.AES128 ) {
+            var key = GenerateKey( password, cipherType );
+
+            var cipher = new AesManaged {
+                KeySize = ( int )cipherType * 8,
+                Key = key,
+                BlockSize = 128,
+                Mode = CipherMode.CBC,
+                IV = aesIV,
+                Padding = PaddingMode.PKCS7
+            };
+
+            using( var ifs = new FileStream( inputFilePath, FileMode.Open, FileAccess.Read ) ) {
+                var ibuf = new byte[ifs.Length];
+                ifs.Read( ibuf, 0, ibuf.Length );
+
+                var obuf = cipher.CreateEncryptor().TransformFinalBlock( ibuf, 0, ibuf.Length );
+
+                using( var ofs = new FileStream( outputFilePath, FileMode.Create, FileAccess.Write ) ) {
+                    var headBuf = BitConverter.GetBytes( ( int )cipherType );
+                    ofs.Write( headBuf, 0, headBuf.Length );
+                    ofs.Write( obuf, 0, obuf.Length );
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypts a file
+        /// </summary>
+        /// <param name="inputFilePath">Encrypted file path</param>
+        /// <param name="outputFilePath">Decrypted file path</param>
+        /// <param name="password">Password</param>
+        public static void Decrypt( string inputFilePath, string outputFilePath, string password ) {
+
+            using( var ifs = new FileStream( inputFilePath, FileMode.Open, FileAccess.Read ) ) {
+                var headBuf = new byte[sizeof( int )];
+                var ibuf = new byte[ifs.Length - headBuf.Length];
+                ifs.Read( headBuf, 0, headBuf.Length );
+                ifs.Read( ibuf, 0, ibuf.Length );
+
+                var cipherType = ( CipherType )BitConverter.ToInt32( headBuf );
+
+                var key = GenerateKey( password, cipherType );
+
+                var cipher = new AesManaged {
+                    KeySize = ( int )cipherType * 8,
+                    Key = key,
+                    BlockSize = 128,
+                    Mode = CipherMode.CBC,
+                    IV = aesIV,
+                    Padding = PaddingMode.PKCS7
+                };
+
+                var obuf = cipher.CreateDecryptor().TransformFinalBlock( ibuf, 0, ibuf.Length );
+
+                using( var ofs = new FileStream( outputFilePath, FileMode.Create, FileAccess.Write ) ) {
+                    ofs.Write( obuf, 0, obuf.Length );
+                }
+            }
+        }
+    }
+}
---
+++ FCrypt/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FCrypt {
+    class Program {
+
+        /// <summary>
+        /// Encrypt or Decrypt
+        /// </summary>
+        enum EncryptOrDecrypt {
+            Unknown,
+            Encrypt,
+            Decrypt
+        }
+
+        static int Main( string[] args ) {
+
+            var (encDec, inputFilePath, outputFilePath, password, cipherType) = ParseCommand( args );
+            if( encDec == EncryptOrDecrypt.Unknown || string.IsNullOrEmpty( inputFilePath ) ) {
+                Console.WriteLine( "Usage: FCrypt -e|-d -i InputFilePath [-o OutputFilePath] [-p Password] [-m aes128|aes256]" );
+                Console.WriteLine();
+                Console.WriteLine( "  -e\t\t\tEncrypt" );
+                Console.WriteLine( "  -d\t\t\tDecrypt" );
+                Console.WriteLine( "  <Encrypt>" );
+                Console.WriteLine( "    -i InputFilePath\tOriginal file path" );
+                Console.WriteLine( "    -o OutputFilePath\tEncrypted file path (If empty, {InputFilePath}.fcrypt)" );
+                Console.WriteLine( "    -p Password\t\tPassword (If empty, it will be prompted for input.)" );
+                Console.WriteLine( "    -m ase128|aes256\t（Encrypt only）AES type (Default: aes128)" );
+                Console.WriteLine( "  <Decrypt>" );
+                Console.WriteLine( "    -i InputFilePath\tEncrypted file path" );
+                Console.WriteLine( "    -o OutputFilePath\tDecrypted file path (If empty, {InputFilePath} without '.fcrypt')" );
+                Console.WriteLine( "    -p Password\t\tPassword (If empty, it will be prompted for input.)" );
+                return 0;
+            }
+
+            if( string.IsNullOrEmpty( outputFilePath ) ) {
+                outputFilePath = inputFilePath;
+            }
+            switch( encDec ) {
+                case EncryptOrDecrypt.Encrypt:
+                    inputFilePath = Path.GetFullPath( inputFilePath );
+                    outputFilePath = !outputFilePath.EndsWith( ".fcrypt", StringComparison.CurrentCulture ) ? $"{Path.GetFullPath( outputFilePath )}.fcrypt" : Path.GetFullPath( outputFilePath );
+                    break;
+                case EncryptOrDecrypt.Decrypt:
+                    inputFilePath = !inputFilePath.EndsWith( ".fcrypt", StringComparison.CurrentCulture ) ? $"{Path.GetFullPath( inputFilePath )}.fcrypt" : Path.GetFullPath( inputFilePath );
+                    outputFilePath = outputFilePath.EndsWith( ".fcrypt", StringComparison.CurrentCulture ) ? Path.GetFullPath( $"./{Path.GetDirectoryName( outputFilePath )}/{Path.GetFileNameWithoutExtension( outputFilePath )}" ) : Path.GetFullPath( outputFilePath );
+                    break;
+            }
+
+            if( !File.Exists( inputFilePath ) ) {
+                Console.Error.WriteLine( $"[ERROR]: Input file not found: {inputFilePath}" );
+                return 1;
+            }
+            if( File.Exists( outputFilePath ) ) {
+                Console.WriteLine( $"Output file is aleady exists: {outputFilePath}" );
+                Console.Write( "Overwrite? [Y]es/[N]o: " );
+                if( Console.ReadLine().ToLower() != "y" ) {
+                    Console.WriteLine( "Quit." );
+                    return 0;
+                }
+            }
+
+            while( string.IsNullOrEmpty( password ) ) {
+                Console.Write( "Password: " );
+                password = InputPassword();
+                Console.WriteLine();
+            }
+
+            try {
+                switch( encDec ) {
+                    case EncryptOrDecrypt.Encrypt:
+                        Console.WriteLine( $"Encrypting file: {inputFilePath}" );
+                        FCrypt.Encrypt( inputFilePath, outputFilePath, password, cipherType );
+                        Console.WriteLine( $"Encrypted -> {outputFilePath}" );
+                        break;
+                    case EncryptOrDecrypt.Decrypt:
+                        Console.WriteLine( $"Decrypting file: {inputFilePath}" );
+                        FCrypt.Decrypt( inputFilePath, outputFilePath, password );
+                        Console.WriteLine( $"Decrypted -> {outputFilePath}" );
+                        break;
+                }
+            }
+            catch( Exception ) {
+                Console.WriteLine( $"Failed to {encDec.ToString()}." );
+            }
+
+            return 0;
+        }
+
+        static readonly Regex cmdReg = new Regex( @"-[iopm]" );
+
+        /// <summary>
+        /// Parses command line argments.
+        /// </summary>
+        /// <param name="args">Command line argments</param>
+        /// <returns></returns>
+        static (EncryptOrDecrypt mode, string inputFilePath, string outputFilePath, string password, CipherType cipherType) ParseCommand( string[] args ) {
+            EncryptOrDecrypt mode = EncryptOrDecrypt.Unknown;
+            string inputFilePath = null;
+            string outputFilePath = null;
+            string password = null;
+            CipherType cipherType = CipherType.AES128;
+
+            string argKey = "";
+            for( int i = 0; i < args.Length; i++ ) {
+                switch( args[i] ) {
+                    case "-e":
+                        if( mode == EncryptOrDecrypt.Unknown ) {
+                            mode = EncryptOrDecrypt.Encrypt;
+                        }
+                        break;
+                    case "-d":
+                        if( mode == EncryptOrDecrypt.Unknown ) {
+                            mode = EncryptOrDecrypt.Decrypt;
+                        }
+                        break;
+                    case "-i":
+                    case "-o":
+                    case "-p":
+                    case "-m":
+                        argKey = args[i];
+                        break;
+                    default:
+                        if( cmdReg.IsMatch( args[i] ) ) {
+                            argKey = args[i].Substring( 0, 2 );
+                            args[i] = args[i].Substring( 2, args[i].Length - 2 );
+                        }
+                        switch( argKey ) {
+                            case "-i":
+                                inputFilePath = args[i];
+                                break;
+                            case "-o":
+                                outputFilePath = args[i];
+                                break;
+                            case "-p":
+                                password = args[i];
+                                break;
+                            case "-m":
+                                cipherType = args[i] == "aes256" ? CipherType.AES256 : CipherType.AES128;
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            return ( mode, inputFilePath, outputFilePath, password, cipherType );
+        }
+
+        /// <summary>
+        /// Inputs a password from console.
+        /// </summary>
+        /// <returns></returns>
+        static string InputPassword() {
+            StringBuilder sb = new StringBuilder();
+            while( true ) {
+                var key = Console.ReadKey( true );
+                if( key.Key == ConsoleKey.Enter ) {
+                    break;
+                }
+                else if( key.Key == ConsoleKey.Backspace && sb.Length > 0 ) {
+                    sb.Remove( sb.Length - 1, 1 );
+                }
+                else if( key.Key == ConsoleKey.Escape ) {
+                    sb.Clear();
+                }
+                else {
+                    sb.Append( key.KeyChar );
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
